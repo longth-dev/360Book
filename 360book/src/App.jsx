033b110c24b08ff14f05a18ee6b77b6e38f7116f
Longnee <longthse184113@fpt.edu.vn>
2025-07-24 10:46:51 +0700

    protectedRoute

--- 360book/src/App.jsx
+++ 360book/src/App.jsx
@@ -61,7 +61,7 @@ function App() {
         <Route path="/hoi-va-dap" element={<CommunityChat />} />
 
         {/** MANAGE ADMIN */}
-        {/* <Route element={<ProtectedRoutesAdmin />}> */}
+        <Route element={<ProtectedRoutesAdmin />}>
         <Route path='/admin' element={<LayoutAdmin />}>
           <Route path="/admin/manage-dai-hoc" element={<ManageUniversity />} />
           <Route path="/admin/universities/:id" element={<ManageUniversityDetail />} />
@@ -70,7 +70,7 @@ function App() {
           <Route path="/admin/manage-to-hop-mon" element={<ManageMajorGroup />} />
           <Route path="/admin/manage-tin-tuc" element={<ManageNews />} />
           <Route path="/admin/diem-chuan" element={<ManageScore />} />
-          {/* </Route> */}
+          </Route>
         </Route>
 
         {/** USER PAGE */}
